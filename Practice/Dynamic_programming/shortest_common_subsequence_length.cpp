5848bd0f4b574b1ecc8c8856383f6e1fbf3e1955
shreya260705 <shreyatpathi2607@gmail.com>
2026-07-09 12:54:01 +0530

    scs

---
+++ Practice/Dynamic_programming/shortest_common_subsequence_length.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        // code here
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return n+m-dp[n][m];
+    }
+};
