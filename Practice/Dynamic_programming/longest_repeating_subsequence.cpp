1ba93095a6574dddde9818e7b88ce7b98e2ec5e4
shreya260705 <shreyatpathi2607@gmail.com>
2026-07-12 17:10:55 +0530

    lrs

---
+++ Practice/Dynamic_programming/longest_repeating_subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int LongestRepeatingSubsequence(string &s) {
+        // Code here
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==s[j-1]&&i!=j){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
