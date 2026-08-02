c08494a4488cc22bc8c4b3433509721ccbe45a6f
shreya260705 <shreyatpathi2607@gmail.com>
2026-08-02 23:12:05 +0530

    middle_index_in_array

---
+++ Practice/find_the_middle_index_in_array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> suffix(n + 1, 0);
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = nums[i] + suffix[i + 1];
+        }
+        int prefix = 0;
+        for (int i = 0; i < n; i++) {
+            if (prefix == suffix[i + 1]) {
+                return i;
+            }
+            prefix += nums[i];
+        }
+        return -1;
+    }
+};
