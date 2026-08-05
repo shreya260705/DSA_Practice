4099377827354e489576988b77f93c460dc56f75
shreya260705 <shreyatpathi2607@gmail.com>
2026-08-05 20:08:12 +0530

    fair candy swap

---
+++ Practice/fair_candy_swap.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& alice, vector<int>& bob) {
+        int sumA = 0, sumB = 0;
+        
+        for (int a : alice) sumA += a;
+        for (int b : bob) sumB += b;
+        
+        int diff = (sumB - sumA) / 2;
+        
+        unordered_set<int> setB(bob.begin(), bob.end());
+        
+        for (int a : alice) {
+            if (setB.count(a + diff)) {
+                return {a, a + diff};
+            }
+        }
+        
+        return {};
+    }
+};
